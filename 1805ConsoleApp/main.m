c99075926ab71fa94e6ca66819f9c55ad3880c85
user <iOS-School-1@iMac-user-8.local>
2017-05-18 19:56:55 +0300

    Initial Commit

---
+++ 1805ConsoleApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  1805ConsoleApp
+//
+//  Created by iOS-School-1 on 18.05.17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
